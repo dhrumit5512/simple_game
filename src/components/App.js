4e47362761c3a582658011f8679bf00b54399aac
dhrumit <dhrumitbarot1@gmail.com>
2023-03-09 06:40:39 -0600

    make a document formate and change some mistake i made by making some capital.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+
 import Game from './Game';
 
 class App extends React.Component {
-    render() {
-        return (
-           <Game randomNumberCounts/>
-        );
-    }
+
+  state = {
+    gameId: 1,
+  };
+  resetGame = () => {
+    this.setState(prevState => {
+      return { gameId: prevState.gameId + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <Game key={this.state.gameId} onPlayAgain={this.resetGame} randomNumberCount={6} intialSeconds={10} />
+    );
+  }
 }
 
 export default App;
